a2d7f379dfa959858175d2d317bfb44d14d1c8df
yu-jin-song <yujinsong.tech@gmail.com>
2021-06-25 06:39:13 +0900

    210625 baekjoon 7-1 11654 아스키 코드

---
+++ yu-jin-song/step7/_11654.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class _11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char c = sc.next().charAt(0);
+		
+		System.out.println((int)c);
+	}
+}
