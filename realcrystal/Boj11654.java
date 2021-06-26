9635923900dc4fd46d924a29ed77beac3f60d0c7
이수정 <realcrystal98@gmail.com>
2021-06-26 23:15:29 +0900

    210626 step7 1/10

---
+++ realcrystal/Boj11654.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char c = br.readLine().charAt(0);
+        System.out.println((int) c);
+    }
+}
