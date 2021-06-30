4ab824337ea5e48e4d5c66d8bf9b31118d45a0eb
이수정 <realcrystal98@gmail.com>
2021-07-01 00:04:44 +0900

    210630 step7 6/10 (문제 번호 수정)

--- realcrystal/Boj1152.java
+++ realcrystal/Boj1152.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Boj1152 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
