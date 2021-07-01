2d4041483d16eec0d5298f2565b7546b2090adf2
yu-jin-song <yujinsong.tech@gmail.com>
2021-07-02 04:45:54 +0900

    210702 baekjoon 8-1 1712 손익분기점 && Change directory structure

---
+++ yu-jin-song/step7/Boj_11654.java
@@ -0,0 +1,13 @@
+package step7;
+
+import java.util.Scanner;
+
+public class Boj_11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char c = sc.next().charAt(0);
+		
+		System.out.println((int)c);
+	}
+}
