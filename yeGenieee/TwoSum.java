7f6554c4170e4893f4feb6910601e83f395d6d2f
yeGenieee <cyj95428@gmail.com>
2021-07-16 22:19:18 +0900

    2021/07/16

---
+++ yeGenieee/TwoSum.java
@@ -0,0 +1,18 @@
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+
+        for (int i=0; i<nums.length; i++) {
+            for (int j=i+1; j<nums.length; j++) {
+                int sum = nums[i] + nums[j];
+
+                if (sum == target) {
+                    answer[0] = i;
+                    answer[1] = j;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
