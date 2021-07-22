4d542d1e5d92d13b06e7ad3b4da9dc77512b566d
yu-jin-song <yujinsong.tech@gmail.com>
2021-07-22 23:59:29 +0900

    210722 baekjoon 9-6 9020 골드바흐의 추측

---
+++ yu-jin-song/step9/Boj_9020.java
@@ -0,0 +1,57 @@
+package step9;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.lang.Math;
+
+public class Boj_9020 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int t = Integer.parseInt(br.readLine());
+		int i, j, k, n, p1=0, p2=0, minus, temp, cnt=0;
+		
+		boolean[] prime = new boolean[10001];
+		ArrayList<Integer> sosu = new ArrayList<>();
+		
+		for(i=2; i<=10000; i++)
+			prime[i] = true;
+		
+		for(i=2; i*i<=10000; i++) {
+			for(j=i*2; j<=10000; j+=i) {
+				if(prime[j])
+					prime[j] = false;
+			}
+		}
+		
+		for(i=2; i<=10000; i++) {
+			if(prime[i])
+				sosu.add(i);
+		}
+		
+		for(i=0; i<t; i++) {
+			n = Integer.parseInt(br.readLine());
+			
+			minus = sosu.get(sosu.size()-1) - sosu.get(0);
+			
+			for(j=2; j<n; j++) {
+				temp = n-j;
+				if(prime[j] && prime[temp] && temp < minus) {
+					minus = temp;
+					p1 = j;
+					p2 = temp;
+				}
+			}
+			
+			bw.write(p1 + " " + p2 + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+}
