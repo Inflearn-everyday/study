d3372a01aa3186323a40cc24a755801201c2e93e
yeGenieee <cyj95428@gmail.com>
2021-07-24 21:56:14 +0900

    2021/07/24

---
+++ yeGenieee/AddStrings.java
@@ -0,0 +1,28 @@
+public class AddStrings {
+    public String addStrings(String num1, String num2) {
+        StringBuilder res = new StringBuilder();
+
+        int carry = 0;
+        int p1 = num1.length() - 1;
+        int p2 = num2.length() - 1;
+
+        while (p1 >= 0 || p2 >= 0) {
+            int x1 = p1 >= 0 ? num1.charAt(p1) - '0' : 0;
+            int x2 = p2 >= 0 ? num2.charAt(p2) - '0' : 0;
+
+            int value = (x1 + x2 + carry) % 10;
+
+            carry = (x1 + x2 + carry) / 10;
+
+            res.append(value);
+            p1--;
+            p2--;
+        }
+
+        if (carry != 0) {
+            res.append(carry);
+        }
+
+        return res.reverse().toString();
+    }
+}
