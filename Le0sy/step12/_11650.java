8b59b2fd37621974f0a7d237dfe49ce15fac5295
Le0sy <leo.sooyeonlee@gmail.com>
2021-07-28 22:22:48 +0900

    210728

---
+++ Le0sy/step12/_11650.java
@@ -0,0 +1,33 @@
+package step12;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class _11650 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (a1, a2) -> {
+            if(a1[0] == a2[0]) return a1[1] - a2[1];
+            else return a1[0] - a2[0];
+        });
+
+        for (int i = 0; i < N; i++) {
+            bw.write(arr[i][0] + " " + arr[i][1] + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
