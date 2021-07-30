50b20680fa7a695ec4bfb061c9d1b715851a3c51
yu-jin-song <yujinsong.tech@gmail.com>
2021-07-30 23:53:00 +0900

    210730 baekjoon 11-1 2798 블랙잭

--- yu-jin-song/step11/Boj_2798.java
+++ yu-jin-song/step11/Boj_2798.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.Arrays;
 
 public class Boj_2798 {
 	public static void main(String[] args) throws IOException {
@@ -23,17 +22,16 @@ public class Boj_2798 {
 		while(st.hasMoreTokens())
 			card[i++] = Integer.parseInt(st.nextToken());
 		
-		Arrays.sort(card);
-		
 		int result = 0;
 		int j, k, max = 0;
 		
-		for(i=0; i<n; i++) {
-			for(j=i+1; j<n; j++) {
-				for(k=j+1; k<n; k++)
+		for(i=0; i<n-2; i++) {
+			for(j=i+1; j<n-1; j++) {
+				for(k=j+1; k<n; k++) {
 					result = card[i] + card[j] + card[k];
-				if(result <= m && result > max)
-					max = result;
+					if (result <= m && result > max)
+						max = result;
+				}
 			}
 		}
 		
