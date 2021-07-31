559850065c1ca907afbfa1af4f953fd6e7c4bfa5
Le0sy <leo.sooyeonlee@gmail.com>
2021-07-31 20:28:31 +0900

    210731

---
+++ Le0sy/step12/_10814.java
@@ -0,0 +1,34 @@
+package step12;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class _10814 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        String[][] arr = new String[N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = st.nextToken();
+            arr[i][1] = st.nextToken();
+        }
+
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i][0] + " " + arr[i][1] + "\n");
+        }
+        System.out.println(sb);
+    }
+}
