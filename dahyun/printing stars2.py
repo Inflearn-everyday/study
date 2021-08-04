27c94858aff5d5237da83dd9049a3ad4e9db40ea
Dahyun <75357330+dahyun1023@users.noreply.github.com>
2021-08-04 06:36:19 -0700

    0804

---
+++ dahyun/printing stars2.py
@@ -0,0 +1,34 @@
+# Code review
+#https://study-all-night.tistory.com/5 
+    
+def draw_star(n) :
+    global Map
+    
+    if n == 3 :
+        Map[0][:3] = Map[2][:3] = [1]*3
+        Map[1][:3] = [1, 0, 1]
+        return
+
+    a = n//3
+    draw_star(n//3)
+    for i in range(3) :
+        for j in range(3) :
+            if i == 1 and j == 1 :
+                continue
+            for k in range(a) :
+                Map[a*i+k][a*j:a*(j+1)] = Map[k][:a] # 핵심 아이디어
+
+N = int(input())      
+
+# 메인 데이터 선언
+Map = [[0 for i in range(N)] for i in range(N)] 
+
+draw_star(N)
+
+for i in Map :
+    for j in i :
+        if j :
+            print('*', end = '')
+        else :
+            print(' ', end = '')
+    print()
