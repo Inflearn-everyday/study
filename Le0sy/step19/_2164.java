b4035a75f96535b1e6c7b553183d9fa6decbc05b
Le0sy <leo.sooyeonlee@gmail.com>
2021-08-08 14:11:27 +0900

    210808

---
+++ Le0sy/step19/_2164.java
@@ -0,0 +1,23 @@
+package step19;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
+        while (q.size() > 1) {
+            q.poll();
+            q.add(q.poll());
+        }
+        System.out.println(q.poll());
+    }
+}
