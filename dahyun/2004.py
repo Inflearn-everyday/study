b98e424896cd1fe79707f80d02cc3b23a2bd0e8d
Dahyun <75357330+dahyun1023@users.noreply.github.com>
2021-08-13 22:34:16 +0900

    08/13/2021

---
+++ dahyun/2004.py
@@ -0,0 +1,20 @@
+  # https://tmdrl5779.tistory.com/95
+
+n, m = map(int, input().split())
+
+
+def two_count(n):
+    two = 0
+    while n != 0:
+        n = n // 2
+        two += n
+    return two
+
+def five_count(n):
+    five = 0
+    while n != 0:
+        n = n // 5
+        five += n
+    return five
+
+print(min(two_count(n) - two_count(n - m) - two_count(m), five_count(n) - five_count(n - m) - five_count(m)))
