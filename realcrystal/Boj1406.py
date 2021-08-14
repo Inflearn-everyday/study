7c741898309602c30cb41c548738822bacc13b66
이수정 <realcrystal98@gmail.com>
2021-08-14 23:59:06 +0900

    2021-08-14 BOJ1406 Editor

---
+++ realcrystal/Boj1406.py
@@ -0,0 +1,12 @@
+sentence = input()
+n = int(input())
+
+for i in range(0, n):
+    command = input()
+    if command[0] == 'P':
+        
+    else if command == 'L':
+        
+    else if command == 'D':
+        
+    else: #B
