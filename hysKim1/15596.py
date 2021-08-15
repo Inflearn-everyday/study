1645af444d4ff3bd9c596fb3fce1e0e19190724f
hysKim1 <42089407+hysKim1@users.noreply.github.com>
2021-08-15 23:48:48 +0900

    20210815

---
+++ hysKim1/15596.py
@@ -0,0 +1,5 @@
+#Python 2, Python 3, PyPy, PyPy3: def solve(a: list) -> int
+#a: 합을 구해야 하는 정수 n개가 저장되어 있는 리스트 (0 ≤ a[i] ≤ 1,000,000, 1 ≤ n ≤ 3,000,000)
+#리턴값: a에 포함되어 있는 정수 n개의 합 (정수)
+def solve(a: list)-> int:
+    return sum(a)
