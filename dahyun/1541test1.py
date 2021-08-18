e2f1f9038bbd91917009374bdbe09fa36d4d8146
Dahyun <75357330+dahyun1023@users.noreply.github.com>
2021-08-18 23:49:17 +0900

    08/18/2021

---
+++ dahyun/1541test1.py
@@ -0,0 +1,9 @@
+list_minus = input().split('-') 
+x = 0 
+
+for i in list_minus[0].split('+'): 
+  x += int(i) 
+  for i in list_minus[1:]: 
+    for j in i.split('+'): 
+      x -= int(j) 
+print(x)
