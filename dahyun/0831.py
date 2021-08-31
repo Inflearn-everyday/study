2245d0bdd0a87ca8c6e458524820b897d2786817
Dahyun <75357330+dahyun1023@users.noreply.github.com>
2021-09-01 01:27:23 +0900

    09/01/2021

--- dahyun/0831.py
+++ dahyun/0831.py
@@ -19,3 +19,19 @@ def sosu(n):
 for _ in range(int(input())):
     n = int(input())
     print(" ".join(map(str, sosu(n))))
+    
+    
+# runtime error 나서
+sosu = [0 for i in range(10001)]
+sosu[1] = 1
+for i in range(2, 98):
+    for j in range(i * 2, 10001, i):
+        sosu[j] = 1
+t = int(input())
+for i in range(t):
+    a = int(input())
+    b = a // 2
+    for j in range(b, 1, -1):
+        if sosu[a - j] == 0 and sosu[j] == 0:
+            print(j, a - j)
+            break
