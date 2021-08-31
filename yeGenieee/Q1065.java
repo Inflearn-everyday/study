17d1e037f80bea286f4393f730d2985a04714713
yeGenieee <cyj95428@gmail.com>
2021-08-31 23:09:03 +0900

    2021/08/31

---
+++ yeGenieee/Q1065.java
@@ -0,0 +1,9 @@
+public class Q1065 {
+
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println("\\(__)|");
+    }
+}
