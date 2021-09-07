2d2d7938fe1262a895583b1e9eaee4f36d5698af
yeGenieee <cyj95428@gmail.com>
2021-09-07 23:47:37 +0900

    2021/09/07

---
+++ yeGenieee/WorkoutClothes.java
@@ -0,0 +1,25 @@
+class WorkoutClothes {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int[] people = new int[n];
+        int answer = n;
+
+        for (int l : lost)
+            people[l-1]--;
+        for (int r : reserve)
+            people[r-1]++;
+
+        for (int i = 0; i < people.length; i++) {
+            if(people[i] == -1) {
+                if(i-1>=0 && people[i-1] == 1) {
+                    people[i]++;
+                    people[i-1]--;
+                }else if(i+1< people.length && people[i+1] == 1) {
+                    people[i]++;
+                    people[i+1]--;
+                }else
+                    answer--;
+            }
+        }
+        return answer;
+    }
+}
