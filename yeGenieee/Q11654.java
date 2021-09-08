1c927884598e74dcbaebf2c85fd19402e54a5016
yeGenieee <cyj95428@gmail.com>
2021-09-08 23:55:17 +0900

    2021/09/08

---
+++ yeGenieee/Q11654.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Q11654 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int ch = scanner.next().charAt(0);
+
+        System.out.println(ch);
+    }
+}
